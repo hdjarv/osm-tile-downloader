f1e3adc38dfdfe6ebb86ff9c75d53c1d1fc8ae0c
Henrik Djärv <henrik@djarv.org>
2015-09-30 13:22:26 +0200

    Initial public release

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # osm-tile-downloader
+
 Script for downloading map tile images from an OSM tile server.
+
+**Note:** This script may cause substantial traffic towards the tile server.
+I advise you to **only** run it against your own [tile server](https://switch2osm.org/serving-tiles/) and not a public one.
+
+## Prerequisites
+
+To run this software you must have [node.js](http://nodejs.org) installed.
+
+## Installation
+
+Use these commands to install this software.
+
+```sh
+git clone https://github.com/hdjarv/osm-tile-downloader.git
+cd osm-tile-downloader/
+npm install
+```
+
+## Using
+
+Run this software with command:
+
+```sh
+./osm-tile-downloader.js <options>
+```
+
+The available options are:
+
+| Name                   | Short name | Required | Description                                                                        |
+|------------------------|------------|----------|------------------------------------------------------------------------------------|
+| --start-zoom-level <n> | -s         | **Yes**  | Starting zoom level, must be between 0 and 19.                                     |
+| --end-zoom-level <n>   | -e         | **Yes**  | Ending zoom level, must be between 0 and 19 and not lower than start zoom level.   |
+| --url <url>            | -u         | **Yes**  | Tile server URL, see more below.                                                   |
+| --output-dir <dir>     | -o         | **Yes**  | Output directory for downloaded tiles, must be an existing directory.              |
+| --verbose              | -v         | *No*     | Verbose mode, default is off.                                                      |
+| --check-tiles          | -c         | *No*     | Check if the expected tiles exist in output directory instead of downloading them. |
+| --delay <ms>           | -d         | *No*     | Delay in ms between downloads, default is 0.                                       |
+| --max-retries <num>    | -m         | *No*     | Maximum number of retries for download, default is 3.                              |
+| --retry-delay <ms>     | -r         | *No*     | Delay in ms between download retries, default is 2500.                             |
+| --force-overwrite      | -f         | *No*     | Force overwriting existing tiles (re-downloads all tiles), default is off.         |
+| --yes                  | -y         | *No*     | Answer yes to prompt confirming downloading of tiles, default is off.              |
+| --help                 | -h         | *No*     | Output usage information.                                                          |
+| --version              | -V         | *No*     | output the version number.                                                         |
+
+
+### Tile server URL requirements
+
+The tile server URL to use must be a complete (`http` or `https`) URL with placeholders `{z}`, `{x}`, `{y}` for zoom level, x- & y-tiles.
+For example: `http://mytileserver.example.com/osm_tiles/{z}/{x}/{y}.png`
---
+++ osm-tile-downloader.js
@@ -0,0 +1,199 @@
+#!/usr/bin/env node
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var readline = require('readline');
+var util = require('util');
+var mkdir = require('mkdirp').sync;
+var moment = require('moment');
+var winston = require('winston');
+var options = require('./lib/options')();
+var pkgInfo = require('./package');
+
+var log = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      level: options.verbose === true ? 'debug' : 'info',
+      timestamp: function () {
+        return moment().format('YYYY-MM-DD HH:mm.ss.SSS');
+      },
+      debugStdout: true,
+      handleExceptions: false
+    })
+  ]
+});
+
+var calculateDownloadCount = function (startZoomLevel, endZoomLevel) {
+  return _.reduce(_.range(startZoomLevel, endZoomLevel + 1), function (result, zoom) {
+    return result + Math.pow(Math.pow(2, zoom), 2);
+  }, 0);
+};
+
+var confirmDownload = function (downloadCount, cb) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question(util.format("Proceed with %s %d tiles? [yes/no] ", options.checkTiles ? 'checking' : 'downloading', downloadCount), function (answer) {
+    if (!(/^(?:y|yes)$/i).test(answer)) {
+      log.info('Cancelled');
+      process.exit(0);
+    }
+    cb();
+    rl.close();
+  });
+};
+
+var downloadTile = function (tile, startZoomLevel, endZoomLevel, retryCount) {
+  retryCount = retryCount || 0;
+  if (!tile) {
+    log.info('Done %s tiles', options.checkTiles ? 'checking' : 'downloading');
+    return;
+  }
+
+  if ((options.forceOverwrite === false && !fs.existsSync(tile.file)) || options.forceOverwrite === true) {
+    if (options.checkTiles) {
+      log.info("Tile '%s' is missing", tile.file);
+      setImmediate(function () {
+        downloadTile(nextTile(startZoomLevel, endZoomLevel, tile), startZoomLevel, endZoomLevel);
+      });
+    }
+    else {
+      log.debug('Download %s', tile.url);
+      mkdir(path.dirname(tile.file));
+      var req = http.request(tile.url, function (resp) {
+        if (resp.statusCode !== 200) {
+          log.error('Unexpected status code: %d (url: %s), retrying in %d ms', resp.statusCode, tile.url, options.retryDelay);
+          setTimeout(function () {
+            if (retryCount < options.maxRetries) {
+              retryCount++;
+              log.info("Retrying %s", tile.url);
+              tiles.unshift(tile);
+            }
+            else {
+              log.info("Skipping tile: %s", tile.url);
+            }
+            downloadTile(nextTile(startZoomLevel, endZoomLevel, tile), startZoomLevel, endZoomLevel, retryCount);
+          }, options.retryDelay);
+        }
+        else {
+          resp.on('error', function (error) {
+            log.error('Error in response', error);
+            process.exit(3);
+          });
+
+          resp.on('end', function () {
+            log.debug('Done saving %s', tile.file);
+            setTimeout(function () {
+              downloadTile(nextTile(startZoomLevel, endZoomLevel, tile), startZoomLevel, endZoomLevel);
+            }, options.delay);
+          });
+
+          log.debug('Saving %s', tile.file);
+          var output = fs.createWriteStream(tile.file);
+          resp.pipe(output);
+        }
+      });
+
+      req.on('error', function (error) {
+        log.error('Error in request', error);
+        process.exit(2);
+      });
+      req.end();
+    }
+  }
+  else {
+    setImmediate(function () {
+      if (!options.checkTiles) {
+        log.debug('Skip downloading %s, already downloaded', tile.url);
+      }
+      downloadTile(nextTile(startZoomLevel, endZoomLevel, tile), startZoomLevel, endZoomLevel);
+    });
+  }
+};
+
+var increaseTileCount = function () {
+  // keeps track of tiles downloaded/checked and outputs progress info for every 10% completed
+  tileCount++;
+  var currentPercentage = Math.floor(Math.floor((tileCount / tilesToDownload) * 100) / 10) * 10;
+  if (currentPercentage !== tileCountPercentage) {
+    tileCountPercentage = currentPercentage;
+    log.info("%d%% complete", tileCountPercentage);
+  }
+};
+
+var nextTile = function (startZoomLevel, endZoomLevel, currentTile) {
+  var createTile = function (zoom, x, y) {
+    increaseTileCount();
+    return {
+      zoom: zoom,
+      x: x,
+      y: y,
+      file: path.join(options.outputDir, zoom.toString(), x.toString(), y.toString()) + '.png',
+      url: options.url.replace(/{z}/g, zoom).replace(/{x}/g, x).replace(/{y}/g, y)
+    }
+  };
+
+  if (!currentTile) {
+    return createTile(startZoomLevel, 0, 0);
+  }
+
+  var zoom = currentTile.zoom, x = currentTile.x, y = currentTile.y;
+  var maxTile = Math.pow(2, zoom) - 1;
+
+  if (x === maxTile && y === maxTile && zoom === endZoomLevel) {
+    return void 0;  // all done return nothing
+  }
+
+  if (x === maxTile && y === maxTile && zoom <= endZoomLevel) {
+    // start over with next zoom level
+    x = 0;
+    y = 0;
+    zoom++;
+  }
+  else if (y === maxTile) {
+    x++;
+    y = 0;
+  }
+  else {
+    y++;
+  }
+
+  return createTile(zoom, x, y);
+};
+
+log.info('OSM Tile Downloader v%s', pkgInfo.version);
+log.info('Start zoom level: %d', options.startZoomLevel);
+log.info('End zoom level: %d', options.endZoomLevel);
+if (!options.checkTiles) {
+  log.info('URL: %s', options.url);
+}
+log.info('Output directory: %s', options.outputDir);
+log.debug('Configuration:');
+log.debug('  Verbose mode: %s', options.verbose ? 'yes' : 'no');
+log.debug('  Check tiles: %s', options.checkTiles ? 'yes' : 'no');
+log.debug('  Force overwrite: %s', options.forceOverwrite ? 'yes' : 'no');
+log.debug('  Yes: %s', options.yes ? 'yes' : 'no');
+log.debug('  Delay: %d ms', options.delay);
+log.debug('  Max retries: %d', options.maxRetries);
+log.debug('  Retry delay: %d ms', options.retryDelay);
+
+var tilesToDownload = calculateDownloadCount(options.startZoomLevel, options.endZoomLevel),
+  tileCount = 0,
+  tileCountPercentage = 0;
+
+var go = function () {
+  log.info("Starting to %s %d tiles", options.checkTiles ? 'check' : 'download', tilesToDownload);
+  downloadTile(nextTile(options.startZoomLevel, options.endZoomLevel), options.startZoomLevel, options.endZoomLevel);
+};
+
+if (options.yes) {
+  go();
+}
+else {
+  confirmDownload(tilesToDownload, go);
+}
